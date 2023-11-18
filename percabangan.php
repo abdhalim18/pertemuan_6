d406d98e6fc825d99457d0804794b96b229252a3
abdhalim18 <mabdulhalim257@gmail.com>
2023-11-18 23:45:23 +0700

    pertemuan_6

---
+++ percabangan.php
@@ -0,0 +1,16 @@
+<?php
+$nilai = 80;
+
+if ($nilai >= 90) {
+    echo "Nilai Anda A";
+} else if ($nilai >= 80) {
+    echo "Nilai Anda B";
+} else if ($nilai >= 70) {
+    echo "Nilai Anda C";
+} else if ($nilai >= 60) {
+    echo "Nilai Anda D";
+} else {
+    echo "Nilai Anda E";
+}
+
+?>
